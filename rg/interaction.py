746a89512d82a830220b73ed9b926592b8903367
sirsh <s.amarteifio14@imperial.ac.uk>
2018-03-28 21:48:49 +0100

    added new composite diagram and graph drawing. added new theory wrapper

--- rg/interaction.py
+++ rg/interaction.py
@@ -1,3 +1,11 @@
+#TODO
+#INTERACTION SYSTEM SHOULD BE FUNCTIONAL AND MULT-THEORY CENTRIC
+#THE RESOLUTION IS WHEN ALL BASE FIELDS ARE KNOWN - ONE CAN CREATE A BASIS - THERE MAY BE RESIDUAL PROLONGATIONS - WE SHOULD CHECK WHICH WERE IN THE ORIGINAL
+#PROBABLY ALL COMMON FIELDS ARE THE ORIGINAL _ BECAUSE I DO NOT THINK WE STOP UNTIL WE FILL IN - BUT NOT REQUIRED
+#SHOULD BE EASIER TO SEE RESOLUTION PATHS; I think 0 happens when one of the original vertices is bypassed when resolving the base fields.
+#
+
+
 #also a class for diagrams that takes interactions or constructions
 #constructions are nested interactions with some more information -a construction can have an effecetive interaction for validation
 #constructions can have k-order loops from 0
@@ -14,6 +22,7 @@ import numpy as np
 import sympy
 from sympy import sympify
 from sympy import Symbol
+from sympy import Matrix
 from sympy import init_printing
 from sympy import latex
 sympy.init_printing()
@@ -51,20 +60,26 @@ class interaction(object):
     ##todo - global system to map field_index to field_letter or new symbol as a configuration option
     def __init__(self, mat, coupling_symbol=None):
         self._mat = mat
+        self._tensor = np.array(mat)
         self.shifted=True
         self.symmetry_factor = 1#not sure if this is the one for the externals??
             
+        
         self.in_fields = []
         self.out_fields = []
         out_ = lambda power,index : Symbol('\\tilde{\phi}_'+str(index))**power
         in_ = lambda power,index : Symbol("\phi_"+str(index) )** power
         ar = np.array(mat)
+        
+        self.num_species = ar.shape[0]
+        
         for s in range(ar.shape[0]):
             self.in_fields.append(in_( ar[(s,0)] , s))
             self.out_fields.append(out_( ar[(s,1)] , s))
             
         #display(Latex(self.__repr__()))
 
+        
     def __common_fields__(self, other):#not sure if this should take cardinality into account - no harm if we are doing gradual reduction
         #easy way to do this would be to subtract them from me. we have count non negative fields in common + the non zero sum
         #SELF - OTHER + 1 summed iun numpy would count preceisely
@@ -72,6 +87,11 @@ class interaction(object):
         ar = (np.array(self._mat) - np.array(other._mat) + 1)
         ar[ar<0]=0
         return ar.sum()
+    
+    @property
+    def tensor(self): 
+        
+        return self._tensor
     
     @property
     def is_trivial(self): return np.array(self._mat).sum() == 1 and np.count_nonzero(self._mat)== 1
@@ -155,6 +175,9 @@ class interaction(object):
     
     def reduce(self, coupled, new_couplings = {}):
         interaction._reduce(self, coupled, new_couplings)
+        
+        #check if all base fields exists in the system and return true or false for resolved
+        
     
     #only coupled can take part at all!!!!! until we find all trivial interactions and then find the final lambdas
     #process is (ANSATZ | DEGENREATE COUPLINGS) | FIND ALL TRIVIAL INTERACTIONS i.e. FIELDS | RESOLVE FINAL 
@@ -179,8 +202,13 @@ class interaction(object):
     
 class interaction_system:
     def __init__(self, interactions):
-        #self._interactions = interactions
-        self.couplings = dict(zip(interactions, [0 for i in range(len(interactions))]))
+        self._interactions = interactions
+        self.__reset__()
+        
+    def __reset__(self):
+        self.couplings = dict(zip(self._interactions, [0 for i in range(len(self._interactions))]))
+        self.num_species = self._interactions[0].num_species #check consistent basis TODO
+        self.theories = []
         
     def __display_couplings__(self):
         return [[a.display(), b] for a,b in self.couplings.items()]
@@ -188,6 +216,7 @@ class interaction_system:
     def __display_interaction_dims__(self):
         return [[a.display(), b] for a,b in self.inverse_couplings.items()]
     
+    #this doesn matter if its the wieght or coupling COUP*WEIGHT=INV_MEASURE so they are adjoint and same diff
     def __invert_coupling__(lamb):
         return 0 if lamb == 0 else INV_MEASURE/lamb
         
@@ -196,6 +225,21 @@ class interaction_system:
         for a,b in chi.items():  d[a] = interaction_system.__invert_coupling__(b)
         return d
         
+    def get_basis(self,scaled=None):
+        items = []
+        for idx, k in enumerate(range(self.num_species)):
+            L = [None,None]
+            ar = np.zeros((self.num_species,2),np.int)
+            ar[k, 0] = 1
+            L[0] = self.inverse_couplings[interaction(ar)]
+            if scaled is not None:  L[0] = L[0]**scaled[k, 0]
+            ar = np.zeros((self.num_species,2),np.int)
+            ar[k, 1] = 1
+            L[1] = self.inverse_couplings[interaction(ar)]
+            if scaled is not None:  L[1] = L[1]**scaled[k, 1]
+            items.append(L)
+        return Matrix(items)
+
     @property
     def inverse_couplings(self): return interaction_system.invert_couplings(self.couplings)
     
@@ -233,14 +277,157 @@ class interaction_system:
                 v2 = interaction_system.__invert_coupling__(CIs[t[1]])
 
                 return (t[0], t[1], v1 * v2)
-        return None,
-        
-
+        return None
     
+    
+    def add_trivial_adjoint_of_known_interactions(self, I):
+        result = None
+        new_couplings = {}
+        for c in self.couplings:
+            res = I % c
+            weight = self.inverse_couplings[I]
+            if res not in list(self.couplings.keys()):
+                if res.is_trivial and res.is_positive:
+                    #having devided my self by something we know, i find the residual is a simple field that is not known
+                    #i have a field dimension and now i will div by the weight if this other field to find the residual weight
+                    #print("trivial:", res.is_trivial, "postive", res.is_positive, "const", res.is_constant, "new:", res ) 
+                    result =  weight/ self.inverse_couplings[c]
+                    #i save it as a new coupling
+                    new_couplings[res] = interaction_system.__invert_coupling__(result)
+        
+        self.couplings.update(new_couplings)
+     
+    def update_theories(self, new_couplings, max_L=None):
+        self.__reset__()
+        self.couplings.update(new_couplings)
+        L = list(self.uncoupled_interactions.keys())
+        max_L = len(L) if max_L == None else max_L
+
+        self.theories = []
+        #TODO - count equations and unknowns - how many ansatx-lmbdas do we need, max combinations there
+        #ACTION TERMS: equations, unknowns= Species*2 i.e. 4 - |NormalAnsataz| therefore TERMS - 4 + X 
+        #One coupling unkown for each equation + costant 4 more. 
+        for i in range(1,max_L):
+            #perms could be used for all orders and test for equiv class
+            for tup in list(itertools.combinations(L,i)):  
+                P= list(tup)
+                nt = new_couplings.copy()
+                for p in P: nt[p] = 1
+                #create a new system - bad design - fix later
+                IS_temp = interaction_system(self._interactions)
+                #use this template of ansatz
+                IS_temp.couplings.update(nt)
+                resolved = IS_temp.reduce(nt)    
+                if resolved:
+                    self.theories.append(nt)
+    
+    def __get_dimensionless_fields_for_theories__(self):
+        fixed = [[] for i in range(len(self.theories))]
+        for i,t in enumerate(self.theories):
+            for f in t.keys():
+                if t[f] == 1:#dimensionless
+                    fixed[i].append(f.display())
+        return fixed
+    
+    @property
+    def __equiv_classes__(self,include_base_fields_ony=True):
+        mat = self.__get_resolved_fields_for_theories__(base_fields_only=include_base_fields_ony)
+        equiv_classes = {}
+        keys = list(mat.keys())
+
+        def is_equiv(a,b):
+            for k in keys:
+                if mat[k][a] != mat[k][b]:return False
+            return True
+        
+        l = len(self.theories)
+        
+        for i in range(l):
+            equiv_classes[i] = [i] #im my own equiv class
+            for t_ex in equiv_classes.keys():
+                #unless im like something upstream, then map to that
+                if is_equiv(i,t_ex) and i != t_ex:  
+                    equiv_classes[i].append(t_ex) 
+                    equiv_classes[t_ex] = equiv_classes[i]
+
+        #go through each theory and put in an equiv class
+        return equiv_classes
+
+    def eval_interaction_for_theory(self, V, idx,should_reduce=True):
+        IS = interaction_system(self._interactions)
+        IS.couplings.update(self.theories[idx])
+        resolved = IS.reduce({})#if we have the base fields i.e. basis - all theories with the same basis are equivalent
+        if resolved:  
+            res = IS.get_basis(np.array(V._mat))
+            if should_reduce: return functools.reduce(operator.mul, res, 1)
+    
+        return False
+                
+    
+    def __get_resolved_fields_for_theories__(self, as_matrix=False, as_powers_of_L=False, base_fields_only=False, show_relevance_at=None, resolve_all=True):
+        ALL = {}
+
+        l = len(self.theories)
+        bases = []
+        
+        theory_couplings = []
+        
+        for index, t in enumerate(self.theories):
+            IS = interaction_system(self._interactions)
+            IS.couplings.update(t)
+            resolved = IS.reduce({})#if we have the base fields i.e. basis - all theories with the same basis are equivalent
+            
+            theory_couplings.append(IS.couplings)
+            
+            if resolved:
+                for c in IS.couplings.keys():
+                    #only base fields to be shown
+                    if base_fields_only and not c.is_trivial: continue
+                        
+                    if c not in ALL:
+                        #place holder for each ordered theory
+                        ALL[c] = [None for i in range(l)]
+                        
+ 
+                    ALL[c][index] = IS.couplings[c]
+                    
+        def get_L_power(s,D=4):
+            try:#using globals in this file
+                if s == None: return None
+                LL = s.subs({T : L**2})
+                return LL.as_powers_dict()[L]
+            except: return 0 #dangerous PLUS need to find out when things are not proper symbols
+
+        def get_rel(s):
+            try:#using globals in this file
+                if s == None: return None
+                LL = s.subs({T : L**2, dim:show_relevance_at})
+                P= LL.as_powers_dict()[L]
+                return P
+            except: return -1 #dangerous PLUS need to
+            
+        if resolve_all:
+            print("resolving for all known fields")
+        
+            for coup in list(ALL.keys()):
+                ALL[coup] =  [interaction_system.__invert_coupling__(self.eval_interaction_for_theory(coup, ind)) for ind in range(len(self.theories))]
+      
+        if as_powers_of_L or show_relevance_at != None:
+            for key in ALL:
+                ls = ALL[key]
+                if show_relevance_at != None: ls = [get_rel(i) for i in ls]
+                else: ls = [get_L_power(i) for i in ls]
+                ALL[key] = ls
+                
+
+            
+
+        return ALL if not as_matrix else Matrix([ [k.display() ] +  ALL[k]  for k in list(ALL.keys())])
+            
     #ideas is to keep trying to find new factors until we fail out
     #we may need to add redundant i.e dimensionless couplings
     #during the process, we should find all trivial interaction couplings which terminates
-    def reduce(self, Chi, order=1):
+    def reduce(self, Chi, order=1, finalize_by_products=True):
         #if len(Chi) == 0: raise ("Must set Chi")
         self.couplings.update(Chi)   
         found_new = True
@@ -258,9 +445,36 @@ class interaction_system:
             res = interaction_system.is_biproduct_of_known_interactions(k, self.coupled_interactions)
             if res != None:#if we find it, we can update the non set guy with the tuple tail which is the product
                 self.couplings[k] = INV_MEASURE/res[-1]
+           
+        for k in self.coupled_interactions:
+            self.add_trivial_adjoint_of_known_interactions(k)  
+            
+        #construct the base field for all species and check that we have a resolved couplings for all
+        for k in range(self.num_species):
+            ar = np.zeros((self.num_species,2),np.int)
+            ar[k, 0] = 1
+            if interaction(ar) not in self.couplings:  return False
+            if self.couplings[interaction(ar)] == 0 : return False #cannot be zero - recall why sometimes 0
+            ar = np.zeros((self.num_species,2),np.int)
+            ar[k, 1] = 1
+            if interaction(ar) not in self.couplings:  return False
+            if self.couplings[interaction(ar)] == 0 : return False #cannot be zero - recall why sometimes 0
+            
+            
+        #if finalize_by_products:
+        #    for k in self.uncoupled_interactions:
+        #        res = self.get_basis(np.array(k._mat))
+        #        self.couplings[k] = INV_MEASURE/res
+                
+        #at the very end we may have got all our base fields but there may be some by-products for which we did not update couplings
+        #we can optinally do this now
+                
+                
+        return True
+            
         
         
-#manage heairchy of nodes
+#manage heirarchy of nodes
 #validator is global and if we can validate it, we can add it to the pile 
 class compound_interaction(object):
     def __init__(self,nodea,nodeb):
